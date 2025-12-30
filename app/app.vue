b617aa894c8c7c19aed34d0f52031780879a9540
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-12-30 19:40:01 +0800

    chore: added minor SEO and og image and favicon

--- app/app.vue
+++ app/app.vue
@@ -9,4 +9,12 @@
     </div>
 </template>
 <script setup lang="ts">
+useSeoMeta({
+    title: 'AP PDF Summarizer',
+    ogTitle: 'Create new scratch file from selection',
+    description: 'Summarizes AP-subject related PDFs for you to your own preference.',
+    ogDescription: 'Summarizes AP-subject related PDFs for you to your own preference.',
+    ogImage: '/og.png',
+    twitterCard: 'summary_large_image',
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-05-15',
+    compatibilityDate: '2025-12-30',
     devtools: {enabled: true},
     future: {
         compatibilityVersion: 4,
