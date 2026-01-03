d87d73a6d9c79de3cff324b66f9bb601263a5a20
Type-32 <87076491+Type-32@users.noreply.github.com>
2026-01-03 18:29:48 +0800

    chore: test using the bun runtime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,4 +37,8 @@ export default defineNuxtConfig({
         pageTransition: { name: 'page', mode: 'out-in' },
         layoutTransition: { name: 'layout', mode: 'out-in' },
     },
+
+    nitro: {
+        preset: 'bun'
+    }
 })
